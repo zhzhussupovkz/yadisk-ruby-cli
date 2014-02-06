1fae132219151df7efe1e205f4ed54ff263f31d0
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2014-02-06 17:47:49 +0600

    add calculating used and available disk space

--- yadisk_api.rb
+++ yadisk_api.rb
@@ -30,6 +30,7 @@ require 'net/https'
 require 'openssl'
 require 'base64'
 require 'digest'
+require 'rexml/document'
 
 #YadiskApi - sipmlest class for working with yandex.disk
 class YadiskApi
@@ -42,14 +43,14 @@ class YadiskApi
   #upload file to the server
   def upload_file options = { :dir => '/', :file => nil }
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     dir = options[:dir]
     file = options[:file]
     req = Net::HTTP::Put.new (dir + file)
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     etag = Digest::MD5.hexdigest File.read(file)
     sha = Digest::SHA256.hexdigest File.read(file)
     size = File.size(file)
@@ -72,14 +73,14 @@ class YadiskApi
   #download file from server
   def download_file options = { :file => nil, :output => nil }
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     file = options[:file]
     output = options[:output]
     req = Net::HTTP::Get.new file
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     req['Host'] = "webdav.yandex.ru"
     req['Accept'] = "*/*"
     req['User-Agent'] = "yadisk-ruby-cli"
@@ -96,12 +97,12 @@ class YadiskApi
   #create directory in the server
   def mkdir dir = nil
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     req = Net::HTTP::Mkcol.new dir
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     req['Host'] = "webdav.yandex.ru"
     req['User-Agent'] = "yadisk-ruby-cli"
     req['Accept'] = "*/*"
@@ -116,14 +117,14 @@ class YadiskApi
   #copy directories and files
   def copy options = { :from => nil, :to => nil }
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     from = options[:from]
     to = options[:to]
     req = Net::HTTP::Copy.new from
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     req['Host'] = "webdav.yandex.ru"
     req['Accept'] = "*/*"
     req['User-Agent'] = "yadisk-ruby-cli"
@@ -139,14 +140,14 @@ class YadiskApi
   #move directories and files
   def move options = { :from => nil, :to => nil }
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     from = options[:from]
     to = options[:to]
     req = Net::HTTP::Move.new from
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     req['Host'] = "webdav.yandex.ru"
     req['Accept'] = "*/*"
     req['User-Agent'] = "yadisk-ruby-cli"
@@ -162,20 +163,54 @@ class YadiskApi
   #remove directory or files from the server
   def rm dir = nil
     url = @api_url
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     req = Net::HTTP::Delete.new dir
-    req.basic_auth(@login, @pass)
+    req.basic_auth @login, @pass
     req['Host'] = "webdav.yandex.ru"
     req['User-Agent'] = "yadisk-ruby-cli"
     req['Accept'] = "*/*"
-    res = http.request(req)
+    res = http.request req
     if res.code == "200"
       puts "Directory #{dir} successfully deleted from the server"
     else
       raise "Directory #{dir} not deleted"
+    end
+  end
+
+  #get used and all disk space
+  def get_space
+    url = @api_url
+    uri = URI.parse url
+    http = Net::HTTP.new uri.host, uri.port
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    quota = '<D:propfind xmlns:D="DAV:">
+      <D:prop>
+      <D:quota-available-bytes/>
+      <D:quota-used-bytes/>
+      </D:prop></D:propfind>'
+    req = Net::HTTP::Propfind.new '/'
+    req.basic_auth @login, @pass
+    req['Host'] = "webdav.yandex.ru"
+    req['User-Agent'] = "yadisk-ruby-cli"
+    req['Accept'] = "*/*"
+    req['Depth'] = "0"
+    req.body = quota
+    res = http.request req
+    if res.code == "207"
+      data = res.body
+      xml = REXML::Document.new data
+      stats = xml.elements['d:multistatus/d:response/d:propstat/d:prop/d:quota-used-bytes']
+      used = stats.to_s.gsub('<d:quota-used-bytes>', '').gsub('</d:quota-used-bytes>', '')
+      stats = xml.elements['d:multistatus/d:response/d:propstat/d:prop/d:quota-available-bytes']
+      available = stats.to_s.gsub('<d:quota-available-bytes>', '').gsub('</d:quota-available-bytes>', '')
+      puts "Used disk space: #{used} bytes"
+      puts "All disk space: #{available} bytes"
+    else
+      raise "Invalid returned data from server"
     end
   end
 
